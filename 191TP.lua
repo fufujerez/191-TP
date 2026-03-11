89028ad59f34afb8f8d89c54a5577a3dc145ef6b
fufujerez <fafabian.zayn@gmail.com>
2026-03-12 05:37:55 +0700

    Create 191TP.lua

---
+++ 191TP.lua
@@ -0,0 +1,94 @@
+local player = game.Players.LocalPlayer
+local UIS = game:GetService("UserInputService")
+local TweenService = game:GetService("TweenService")
+
+-- GUI
+local ScreenGui = Instance.new("ScreenGui")
+ScreenGui.Parent = player:WaitForChild("PlayerGui")
+
+-- Frame utama
+local Frame = Instance.new("Frame")
+Frame.Parent = ScreenGui
+Frame.Size = UDim2.new(0,200,0,150)
+Frame.Position = UDim2.new(0.05,0,0.3,0)
+Frame.BackgroundColor3 = Color3.fromRGB(0,0,0)
+Frame.Active = true
+Frame.Draggable = true
+
+-- Title
+local Title = Instance.new("TextLabel")
+Title.Parent = Frame
+Title.Size = UDim2.new(1,0,0,30)
+Title.BackgroundTransparency = 1
+Title.Text = "191 TP"
+Title.TextColor3 = Color3.fromRGB(255,255,255)
+Title.TextScaled = true
+
+-- Button TP Bahan
+local BtnBahan = Instance.new("TextButton")
+BtnBahan.Parent = Frame
+BtnBahan.Size = UDim2.new(1,-20,0,40)
+BtnBahan.Position = UDim2.new(0,10,0,40)
+BtnBahan.Text = "TP Marshmallow Bahan"
+BtnBahan.BackgroundColor3 = Color3.fromRGB(40,40,40)
+BtnBahan.TextColor3 = Color3.fromRGB(255,255,255)
+
+-- Button TP RS
+local BtnRS = Instance.new("TextButton")
+BtnRS.Parent = Frame
+BtnRS.Size = UDim2.new(1,-20,0,40)
+BtnRS.Position = UDim2.new(0,10,0,90)
+BtnRS.Text = "TP RS"
+BtnRS.BackgroundColor3 = Color3.fromRGB(40,40,40)
+BtnRS.TextColor3 = Color3.fromRGB(255,255,255)
+
+-- Function TP
+function TP_MS_BAHAN()
+    player.Character.HumanoidRootPart.CFrame = CFrame.new(523.52,3.59,615.07)
+end
+
+function TP_RS()
+    player.Character.HumanoidRootPart.CFrame = CFrame.new(1063.96,3.36,438.53)
+end
+
+BtnBahan.MouseButton1Click:Connect(TP_MS_BAHAN)
+BtnRS.MouseButton1Click:Connect(TP_RS)
+
+-- Tween settings
+local openSize = UDim2.new(0,200,0,150)
+local closedSize = UDim2.new(0,200,0,30)
+
+local tweenInfo = TweenInfo.new(
+    0.35,
+    Enum.EasingStyle.Quad,
+    Enum.EasingDirection.Out
+)
+
+local minimized = false
+
+-- Minimize dengan Z
+UIS.InputBegan:Connect(function(input,gp)
+    if gp then return end
+    
+    if input.KeyCode == Enum.KeyCode.Z then
+        minimized = not minimized
+        
+        if minimized then
+            BtnBahan.Visible = false
+            BtnRS.Visible = false
+            
+            TweenService:Create(Frame,tweenInfo,{
+                Size = closedSize
+            }):Play()
+            
+        else
+            TweenService:Create(Frame,tweenInfo,{
+                Size = openSize
+            }):Play()
+            
+            task.wait(0.25)
+            BtnBahan.Visible = true
+            BtnRS.Visible = true
+        end
+    end
+end)
